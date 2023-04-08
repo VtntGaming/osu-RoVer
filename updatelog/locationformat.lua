734bb8adaf3f1d7c96676551410314b199c25e13
VtntGaming <82591379+VtntGaming@users.noreply.github.com>
2023-04-09 00:48:31 +0700

    Update locationformat.lua

--- updatelog/locationformat.lua
+++ updatelog/locationformat.lua
@@ -1,6 +1,7 @@
 local Log = {
 	URL = "https://raw.githubusercontent.com/VtntGaming/osu-RoVer/",
 	Location = {
+		{"V1.44","main/updatelog/V1_44.txt"},
 		{"V1.43","main/updatelog/V1_43.txt"},
 		{"V1.42","main/updatelog/V1_42.txt"},
 		{"V1.41","main/updatelog/V1_41.txt"},
